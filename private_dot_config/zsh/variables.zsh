e3e7be6d7918e082cb411bd564fabd80b2762452
Hfreitas <hfreitas86@gmail.com>
2022-11-24 15:38:59 -0300

    fix: correção de comando fzf

--- private_dot_config/zsh/variables.zsh
+++ private_dot_config/zsh/variables.zsh
@@ -1,4 +1,4 @@
 export PROJECTS_FOLDER="${HOME}/Documents/projects"
 export CHROME_EXECUTABLE="/usr/bin/google-chrome-stable"
-export FZF_DEFAULT_COMMAND="fd --type f"
+export FZF_DEFAULT_COMMAND="fdfind --type f"
 
